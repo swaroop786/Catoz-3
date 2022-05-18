3fcd575e9f0ff0fe7552fd8e2481722082968862
charankumarpalla <chrankumar.palla@gmail.com>
2022-05-18 19:36:06 +0530

    Changes delivered for https://github.com/Catoz-3/AdavithaBank/issues/1

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+
+const internetbanking = require('./routes/internetbanking/internetbanking.js')
+const loan = require('./routes/loan/loan.js')
+const shopping = require('./routes/shopping/shopping.js')
+const creditcard = require('./routes/creditcard/creditcard.js')
+
+
+app.use(express.static("public"));
+
+app.use('/internetbanking', internetbanking)
+app.use('/loan', loan)
+app.use('/shopping', shopping)
+app.use('/creditcard', creditcard)
+
+
+const port = process.env.PORT || 8080;
+
+
+app.listen(port, () => {
+    console.log("running on port 8080");
+})
